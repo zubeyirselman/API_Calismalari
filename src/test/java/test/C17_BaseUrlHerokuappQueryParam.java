8ef499e84b8d5ef2d6a974d0518e088846c31c0c
zsk <zsk199207@gmail.com>
2023-07-03 23:56:57 +0300

    21.06.23

---
+++ src/test/java/test/C17_BaseUrlHerokuappQueryParam.java
@@ -0,0 +1,120 @@
+package test;
+
+import baseUrl.HerokuAppBaseURL;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+public class C17_BaseUrlHerokuappQueryParam extends HerokuAppBaseURL {
+
+    // Class icinde 3 Test metodu olusturun ve asagidaki testleri yapin.
+
+    /*
+        1-  https://restful-booker.herokuapp.com/booking endpointine bir GET
+        request gonderdigimizde donen response’ un status code’ unun 200 oldugunu
+        ve Response’ ta 33071 id' ye sahip bir booking oldugunu test edin.
+     */
+
+     /*
+        2- https://restful-booker.herokuapp.com/booking endpointine gerekli
+        Query parametrelerini yazarak “firstname” degeri “Eric” olan rezervasyon
+        oldugunu test edecek bir GET request gonderdigimizde, donen response’ un
+        status code’ unun 200 oldugunu ve “Eric” ismine sahip en az bir booking oldugunu test edin.
+    */
+
+    /*
+        3- https://restful-booker.herokuapp.com/booking endpointine gerekli Query
+         parametrelerini yazarak “firstname” degeri “Jim” ve “lastname” degeri
+         “Jackson” olan rezervasyon oldugunu test edecek bir GET request gonderdigimizde,
+         donen response’ un status code’ unun 200 oldugunu ve “Jim Jackson” ismine sahip
+         en az bir booking oldugunu test edin.
+    */
+
+    @Test
+    public void get01(){
+
+        /*
+        1-  https://restful-booker.herokuapp.com/booking endpointine bir GET
+        request gonderdigimizde donen response’ un status code’ unun 200 oldugunu
+        ve Response’ ta 33071 id' ye sahip bir booking oldugunu test edin.
+        */
+
+        // 1 - Url hazirla
+
+        specHerokuApp.pathParam("pp1","booking");
+
+        // 2 - Response' i kaydet
+
+        Response response = given().spec(specHerokuApp).when().get("/{pp1}");
+        response.prettyPrint();
+
+        // 3 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("bookingid", Matchers.hasItem(1072));
+    }
+
+    @Test
+    public void get02(){
+
+        /*
+        2- https://restful-booker.herokuapp.com/booking endpointine gerekli
+        Query parametrelerini yazarak “firstname” degeri “Eric” olan rezervasyon
+        oldugunu test edecek bir GET request gonderdigimizde, donen response’ un
+        status code’ unun 200 oldugunu ve “Eric” ismine sahip en az bir booking oldugunu test edin.
+        */
+
+        // 1 - Url hazirla
+
+        specHerokuApp.pathParam("pp1","booking").queryParam("firstname","Eric");
+
+        // 2 - Response' i kaydet
+
+        Response response = given().spec(specHerokuApp).when().get("/{pp1}");
+        response.prettyPrint();
+
+        // 3 - Assertion
+
+        response.then().assertThat().statusCode(200).body("bookingid",hasSize(3));
+
+    }
+
+    @Test
+    public void get03(){
+
+        /*
+        3- https://restful-booker.herokuapp.com/booking endpointine gerekli Query
+         parametrelerini yazarak “firstname” degeri “Jim” ve “lastname” degeri
+         “Jackson” olan rezervasyon oldugunu test edecek bir GET request gonderdigimizde,
+         donen response’ un status code’ unun 200 oldugunu ve “Jim Jackson” ismine sahip
+         en az bir booking oldugunu test edin.
+    */
+
+        // 1 - Url hazirla
+
+        specHerokuApp.pathParam("pp1","booking")
+                     .queryParams("firstname","Jim","lastname","Jackson");
+
+        // 2 - Response' i kaydet
+
+        Response response = given().spec(specHerokuApp).when().get("/{pp1}");
+
+        response.prettyPrint();
+
+        // 3 - Assertion
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("bookingid",hasSize(1));
+    }
+
+}
